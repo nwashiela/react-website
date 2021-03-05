ee989c41ee569fb4c8a6b8668a959d99fa24b0dc
nwashiela <nwmahela12@gmail.com>
2021-03-05 10:35:35 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        <h1>Baristana coffee prep</h1>
+        We are Baristanas, we have been dadicated and pationate in every cup,since 2019.
+      </header>
+      <container>
+ <p> our Coffes are as follow: </p>
+ <h2> Caramel Macchiato</h2>
+      <img src="/caramel-macchiato.jpeg" alt="coffe"/>
+      <h2> Frappecino</h2>
+      <img src="/frappecino.jpg" alt="coffe"/>
+      <h2>Cafe Latte</h2>
+      <img src="/CAFÉ-LATTE.jpeg" alt="coffe"/>
+      </container>
+      <container2>
+         <h1>BEST SELLERS </h1>
+         Order now in our side/ can make a call on 021 256 3698 
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    </container2>
+    <footer> email: barista.stanas@gmail.com </footer>
+    </div>
+  );
+}
+
+export default App;
